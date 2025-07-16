16c85d8fd8b2c4843d45523067c4cd03ad1926f4
ligonnierefranck <franckligonniere1@gmail.com>
2025-07-16 12:05:21 +0200

    add plugin for automatique title

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,9 +1,14 @@
-import { defineConfig } from 'vitepress';
+import { defineConfig , Plugin } from 'vitepress';
 import { generateSidebar } from './sidebar-generator.js';
 
 export default defineConfig({
   title: 'Support Deemply',
   description: 'Documentation d\'aide pour Deemply',
+  markdown: {
+    config(md) {
+      md.use(injectTitlePlugin)
+    }
+  },
   themeConfig: {
     logo: '/images/logo.png',
     nav: [
@@ -23,3 +28,21 @@ export default defineConfig({
   lastUpdated: true,
   cleanUrls: true,
 });
+
+const injectTitlePlugin = {
+    name: 'inject-title-h1',
+    transform(code, id, options) {
+      // Ne traite que les fichiers .md avec frontmatter
+      if (!id.endsWith('.md') || !options?.frontmatter?.title) return
+  
+      const title = options.frontmatter.title.trim()
+      const lines = code.split('\n')
+  
+      // Si un titre H1 existe déjà, on ne fait rien
+      const alreadyHasH1 = lines.find((line) => line.trim().startsWith('# '))
+      if (alreadyHasH1) return
+  
+      // Injecte le titre au début
+      return `# ${title}\n\n${code}`
+    }
+  }
--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -2,6 +2,7 @@ import DefaultTheme from 'vitepress/theme'
 
 export default {
   ...DefaultTheme,
+
   // si tu as un layout custom :
   Layout: DefaultTheme.Layout,
 }
