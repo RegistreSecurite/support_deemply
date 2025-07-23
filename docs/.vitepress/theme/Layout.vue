51baf9440523dfddd4e497c273ec5f74447dd0d9
ligonnierefranck <franckligonniere1@gmail.com>
2025-07-23 14:19:44 +0200

    update theme

---
+++ docs/.vitepress/theme/Layout.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="layout">
+      <h1>{{ frontmatter.title }}</h1>
+      <Content />
+    </div>
+  </template>
+  
+  <script setup>
+  import { useData } from 'vitepress'
+  const { frontmatter } = useData()
+  </script>
+  
